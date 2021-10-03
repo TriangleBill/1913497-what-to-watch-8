bafc920be473b53ab5b2b759bceb907442003498
Triangle_Bill <d1demond1@gmail.com>
2021-10-03 18:08:37 +0300

    Добавил в проект остальные страницы и исправил ошибки Eslint

--- project/src/components/main/card.tsx
+++ project/src/components/main/card.tsx
@@ -1,13 +1,17 @@
+type cardProps = {
+  filmName: string,
+  previewImage: string
+}
 
-export default function Card(): JSX.Element {
+export default function Card({filmName, previewImage }: cardProps): JSX.Element {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src="img/we-need-to-talk-about-kevin.jpg" alt="We need to talk about Kevin" width="280" height="175" />
+        <img src={previewImage} alt={filmName} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">We need to talk about Kevin</a>
+        <a className="small-film-card__link" href="film-page.html">{filmName}</a>
       </h3>
     </article>
-  )
+  );
 }
