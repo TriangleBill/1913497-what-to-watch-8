09c08ed024a7e1b004a7e0dcc6781d71426b28b1
Triangle_Bill <d1demond1@gmail.com>
2021-10-20 19:28:15 +0300

    Реализовал табы на странице фильма

---
+++ project/src/components/tabs/tab-overview.tsx
@@ -0,0 +1,23 @@
+import { FilmsDescription } from "../../types/films"
+
+type TabOverviewProps = {
+    film: FilmsDescription
+}
+
+export default function TabOverview(props:TabOverviewProps) {
+    return (
+        <>
+            <div className="film-rating">
+                <div className="film-rating__score">{props.film.rating}</div>
+                <p className="film-rating__meta">
+                    <span className="film-rating__level">Very good</span>
+                    <span className="film-rating__count">240 ratings</span>
+                </p>
+            </div>
+
+            <div className="film-card__text">
+                {props.film.description}
+            </div>
+        </>
+    )
+}
---
+++ project/src/components/tabs/tabs.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { FilmsDescription } from "../../types/films"
+import TabsContent from "./tabs-content";
+
+type TabsProps = {
+    film: FilmsDescription
+}
+
+export default function Tabs(props: TabsProps): JSX.Element {
+    const [activeTab, setActiveTab] = useState("Overview");
+    const titles = ['Overview', 'Details', 'Reviews']
+
+    function onClick (e:any) {
+        setActiveTab(e.target.innerText)
+    }
+
+    return (
+        <>
+            <div className="film-card__desc">
+                <nav className="film-nav film-card__nav">
+                    <ul className="film-nav__list">
+                        {titles.map((el, index)=> (
+                            <li className={`film-nav__item ${activeTab===el ? 'film-nav__item--active' : null}`}>
+                            <div style={{cursor: 'pointer'}} className="film-nav__link" onClick={onClick}>{el}</div>
+                        </li>
+                        ))}
+
+ 
+                    </ul>
+                </nav>
+
+                <TabsContent film={props.film} activeTab={activeTab}/>
+
+
+                {/* <div className="film-rating">
+                    <div className="film-rating__score">{props.film.rating}</div>
+                    <p className="film-rating__meta">
+                        <span className="film-rating__level">Very good</span>
+                        <span className="film-rating__count">240 ratings</span>
+                    </p>
+                </div>
+
+                <div className="film-card__text">
+                    {props.film.description}
+                </div> */}
+
+
+
+            </div>
+        </>
+    )
+}
