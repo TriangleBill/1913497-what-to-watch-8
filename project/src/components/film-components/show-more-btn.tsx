b17111e521ad31bcab68400149c551a809cc3c1f
Triangle_Bill <d1demond1@gmail.com>
2021-11-14 23:33:25 +0300

    Избавился от рефа в кнопке show more

--- project/src/components/film-components/show-more-btn.tsx
+++ project/src/components/film-components/show-more-btn.tsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFilterFilms, getShownFilms } from './../../store/films-process/selector';
 import { incrementShownFilms } from './../../store/action';
@@ -6,7 +5,6 @@ import { incrementShownFilms } from './../../store/action';
 
 export default function ShowMoreBtn(): JSX.Element {
   const dispatch = useDispatch();
-  const btnElement = useRef<HTMLButtonElement>(null);
   const cardElements = useSelector(getFilterFilms);
   const shownFilms = useSelector(getShownFilms);
 
@@ -15,21 +13,13 @@ export default function ShowMoreBtn(): JSX.Element {
     dispatch(incrementShownFilms());
   }
 
-  if  (btnElement.current !== null) {
-    if (isBtnHide()) {btnElement.current.style.visibility = 'hidden';}
-    else  {btnElement.current.style.visibility = 'visible';}
-
-  }
-
-  function isBtnHide() {
-    if (cardElements.length < shownFilms) {
-      return true;
-    } else {return false;}
+  if (cardElements.length < shownFilms) {
+    return <div />;
   }
 
   return (
     <div className="catalog__more">
-      <button ref={btnElement} className="catalog__button" type="button" onClick={onClick}>Show more</button>
+      <button className="catalog__button" type="button" onClick={onClick}>Show more</button>
     </div>
   );
 }
